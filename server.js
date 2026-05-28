f15271b03877ce7166e52da9fae4d641a03c8e59
SHreyansh11234 <106551@nd.balbharati.org>
2026-05-28 22:49:58 +0530

    fix: map /about.html and /index.html routes to prevent 404s on Vercel

--- server.js
+++ server.js
@@ -27,7 +27,15 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/about.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'about.html'));
 });
 
